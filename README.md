6f89e77792c4f6c5f0e26f3bfbcf6ddb27008a36
Vikas Kumar <vkumar@codeplayers.co>
2020-12-08 22:32:42 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Dynamic-BS5-Modal-Box
 Create Bootstrap 5 Modal Box using JavaScript with custom title, description, button labels and custom YES button callback
+
+## Install
+```html
+<!-- Bootstrap 5 CSS -->
+<link 
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+rel="stylesheet"
+integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+crossorigin="anonymous">
+
+<!-- Bootstrap 5 JS -->
+<script 
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+crossorigin="anonymous"></script>
+
+<!-- Dynamic Modal JS -->
+<script src="modal-with-custom-action.js"></script>
+```
+## Usage
+```js
+// showModal(title, description, yesBtnLabel = 'Yes', noBtnLabel = 'Cancel', callbackAfterClickingYesBtn);
+document.getElementById('btn1').onclick = () => showModal('File Deletion', 'Do you want to delete this file?', "Yes", "No", () => {
+    console.log('File deleted successfully');
+});
+```
+![screenshot](assets/screenshot.png)
---
+++ modal-with-custom-action.js
@@ -0,0 +1,45 @@
+// Learn Template literals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+// Learn about Modal: https://getbootstrap.com/docs/5.0/components/modal/
+
+var modalWrap = null;
+/**
+ * 
+ * @param {string} title 
+ * @param {string} description content of modal body 
+ * @param {string} yesBtnLabel label of Yes button 
+ * @param {string} noBtnLabel label of No button 
+ * @param {function} callback callback function when click Yes button
+ */
+const showModal = (title, description, yesBtnLabel = 'Yes', noBtnLabel = 'Cancel', callback) => {
+  if (modalWrap !== null) {
+    modalWrap.remove();
+  }
+
+  modalWrap = document.createElement('div');
+  modalWrap.innerHTML = `
+    <div class="modal fade" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header bg-light">
+            <h5 class="modal-title">${title}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>${description}</p>
+          </div>
+          <div class="modal-footer bg-light">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">${noBtnLabel}</button>
+            <button type="button" class="btn btn-primary modal-success-btn" data-bs-dismiss="modal">${yesBtnLabel}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  modalWrap.querySelector('.modal-success-btn').onclick = callback;
+
+  document.body.append(modalWrap);
+
+  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
+}
